a79a64867c6be3b05200adc45ab7970f60998c60
Ritesh <codeshorts007@gmail.com>
2023-12-18 17:05:44 +0530

    backend: feat: user schema

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+  },
+});
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
