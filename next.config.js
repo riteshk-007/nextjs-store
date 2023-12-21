7a6076bf0c5785b922af8815fda7dd8d0d1ae61d
Ritesh <codeshorts007@gmail.com>
2023-12-21 18:56:54 +0530

    frontend: style: about page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["tailwindui.com", "images.unsplash.com"],
+    domains: ["tailwindui.com", "images.unsplash.com", "i.ibb.co"],
   },
 };
 
