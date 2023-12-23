f8e8f9a4c50c7f28fb067066d376f8ac8706cfb2
Ritesh <codeshorts007@gmail.com>
2023-12-23 12:38:27 +0530

    frontend: style: signle product  page design

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["tailwindui.com", "images.unsplash.com", "i.ibb.co"],
+    domains: [
+      "tailwindui.com",
+      "images.unsplash.com",
+      "i.ibb.co",
+      "i.postimg.cc",
+    ],
   },
 };
 
