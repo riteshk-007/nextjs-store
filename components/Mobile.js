8a7dd5c5392d06781be9cf5e330ca33f540bb36d
Ritesh <codeshorts007@gmail.com>
2023-12-27 15:00:03 +0530

    frontend: fix: mobile admin link & view

--- components/Mobile.js
+++ components/Mobile.js
@@ -42,7 +42,17 @@ const Mobile = ({ setIsOpen }) => {
                 Home
               </Link>
             </li>
-
+            {user?.data?.isAdmin && (
+              <li>
+                <Link
+                  href="/dashboard"
+                  onClick={() => setIsOpen(false)}
+                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                >
+                  Admin
+                </Link>
+              </li>
+            )}
             <li>
               <details className="group [&_summary::-webkit-details-marker]:hidden">
                 <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
