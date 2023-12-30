cc1aa4607bce84f2e736c6a48e489624b4308a0e
Ritesh <codeshorts007@gmail.com>
2023-12-30 17:31:51 +0530

    backend: fix: add to cart route

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -5,19 +5,30 @@ import { NextResponse } from "next/server";
 export const POST = async (req) => {
   await connectDB();
   const { userId, items } = await req.json();
+
   try {
-    const cartItem = await Cart.create({ userId, items });
-    return NextResponse.json({
-      message: "Item added successfully",
-      cartItem,
-      status: 201,
+    let cart = await Cart.findOne({
+      userId,
+      "items.productId": items[0].productId,
     });
+
+    if (cart) {
+      return NextResponse.json({
+        message: "Item already added",
+      });
+    } else {
+      cart = await Cart.create({ userId, items });
+      return NextResponse.json({
+        message: "Item added",
+        cart,
+      });
+    }
   } catch (error) {
     console.log(error);
+
     return NextResponse.json({
       message: "Item not added",
       error,
-      status: 500,
     });
   }
 };
